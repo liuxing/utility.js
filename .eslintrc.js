f9973203c1ca412eccaee215b7cf46d56a4aa499
xing liu <ogilhinn@gmail.com>
2017-08-18 16:30:02 +0800

    :tada: Utility.js init

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "rules": {
+        "semi": ["error", "never"],
+        "quotes": ["error", "single"],
+        "no-unused-vars": 0,
+        "no-console": 0,
+        "indent": ['error', 4],
+        "no-param-reassign": 0,
+    }
+};
